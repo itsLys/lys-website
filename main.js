90a653d792505e3d98cc07df5446fe01f9cb8d21
Lys <153897863+itsLys@users.noreply.github.com>
2024-01-29 13:19:34 +0100

    feat: Add scroll up button using JS

--- main.js
+++ main.js
@@ -56,3 +56,17 @@ $(document).ready(function () {
     } // End if
   });
 });
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+

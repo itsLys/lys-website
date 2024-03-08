9553c5ab2e5168e8ddb4ab210254173185b7c870
Lys <elyesseen@gmail.com>
2024-03-08 10:42:15 +0100

    Add night theme and toggle

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
 // Contact form
 function success() {
-  const successMessage = document.getElementById('message-sent');
-  successMessage.textContent = 'Message sent successfully!'
+  const successMessage = document.getElementById("message-sent");
+  successMessage.textContent = "Message sent successfully!";
 }
 function error() {
-  const successMessage = document.getElementById('message-sent');
-  successMessage.textContent = 'Something went wrong!'
+  const successMessage = document.getElementById("message-sent");
+  successMessage.textContent = "Something went wrong!";
 }
 window.onload = function () {
-  document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    // generate a five digit number for the contact_number variable
-    this.contact_number.value = Math.random() * 100000 | 0;
-    // these IDs from the previous steps
-    emailjs.sendForm('service_mdq1zw3', 'template_40f1q5o', this)
-      .then(function () {
-        console.log('SUCCESS!');
-        success()
-      }, function (error) {
-        error();
-        console.log('FAILED...', error);
-      });
-  });
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_mdq1zw3", "template_40f1q5o", this).then(
+        function () {
+          console.log("SUCCESS!");
+          success();
+        },
+        function (error) {
+          error();
+          console.log("FAILED...", error);
+        }
+      );
+    });
 };
 // Nav Toggle
 (function () {
-  var button = document.getElementById('nav-toggle');
-  button.addEventListener('click', function (event) {
+  var button = document.getElementById("nav-toggle");
+  button.addEventListener("click", function (event) {
     event.preventDefault();
-    var menu = document.getElementById('nav-list');
-    menu.classList.toggle('open');
+    var menu = document.getElementById("nav-list");
+    menu.classList.toggle("open");
   });
 })();
 $(document).ready(function () {
   // Add smooth scrolling to all links
-  $("a").on('click', function (event) {
-
+  $("a").on("click", function (event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -46,13 +49,16 @@ $(document).ready(function () {
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function () {
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
     } // End if
   });
 });
@@ -60,13 +66,23 @@ $(document).ready(function () {
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
+const nightToggle = document.querySelector("#night-btn");
+nightToggle.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.body.classList.toggle("night");
+});
